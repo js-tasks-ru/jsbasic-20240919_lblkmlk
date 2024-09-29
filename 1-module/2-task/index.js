12c96048ef53c9b7514c36615aadc6785cff01f5
ананас <ananas@MacBook-Air.local>
2024-09-29 20:27:08 +0300

    вполнила задачи второго модуля и исправила задачи первого

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,10 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (name && name.length >= 4 && name.indexOf(' ') === -1) {
-    return true;
-  }
-  return false;
+  return name !== null && name.length >= 4 && !name.includes(' ');
 }
 
 function sayHello() {
