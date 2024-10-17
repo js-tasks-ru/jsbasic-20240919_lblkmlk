34cb4b1dfb207b202bd4b4fca6b593447e24ea83
ананас <ananas@MacBook-Air.local>
2024-10-17 18:50:24 +0300

    выполнила задачи из 5 и 6 занятий

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  const leftArrow = document.getElementsByClassName('carousel__arrow_left')[0];
+  const rightArrow = document.getElementsByClassName('carousel__arrow_right')[0];
+  const tape = document.getElementsByClassName('carousel__inner')[0];
+  const updateArrows = () => {
+    leftArrow.style.display = '';
+    rightArrow.style.display = '';
+
+    if (posIndex === 0) {
+      leftArrow.style.display = 'none';
+    }
+    else if (posIndex === 3) {
+      rightArrow.style.display = 'none';
+    }
+  }
+
+  let posIndex = 0;
+
+  updateArrows();
+
+  leftArrow.onclick = () => {
+    posIndex--;
+    tape.style.transform = `translateX(${posIndex * -tape.offsetWidth}px)`;
+    updateArrows();
+  };
+  rightArrow.onclick = () => {
+    posIndex++;
+    tape.style.transform = `translateX(${posIndex * -tape.offsetWidth}px)`;
+    updateArrows();
+  };
+
+
+
 }
